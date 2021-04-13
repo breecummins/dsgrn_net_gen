b2a59e908362f5b8f05fd563f0342a26e9b5526c
Bree Cummins <breecummins@gmail.com>
2021-04-13 13:16:27 -0600

    changed local dependency to dsgrn_utilities dependency

--- src/dsgrn_net_gen/__init__.py
+++ src/dsgrn_net_gen/__init__.py
@@ -1,7 +1,6 @@
 import dsgrn_net_gen.fileparsers
-import dsgrn_net_gen.graphtranslation
 import dsgrn_net_gen.makejobs
 import dsgrn_net_gen.networksearch
 import dsgrn_net_gen.filters
 
-__all__ = ["fileparsers","graphtranslation","makejobs","networksearch","filters"]
+__all__ = ["fileparsers","makejobs","networksearch","filters"]
